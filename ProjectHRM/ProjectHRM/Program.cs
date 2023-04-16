4001e58bc75e8d144ca6c4a9d579fb4d66343ec8
HT-Tuan <huynhthanhtuan2206@gmail.com>
2023-04-16 22:35:11 +0700

    them giao dien trang chu

--- ProjectHRM/ProjectHRM/Program.cs
+++ ProjectHRM/ProjectHRM/Program.cs
@@ -16,7 +16,8 @@ namespace ProjectHRM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TrangChuHRM());
         }
+
     }
 }
