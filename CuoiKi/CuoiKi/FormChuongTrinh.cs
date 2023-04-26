309c3b5a310d8e295c1fd97a6c489d2de4ad7c37
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-04-26 22:49:13 +0700

    up

---
+++ CuoiKi/CuoiKi/FormChuongTrinh.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuoiki
+{
+    public partial class FormChuongTrinh : Form
+    {
+        bool isThoat = true;
+        //Fields
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+        //Constructor
+        public FormChuongTrinh()
+        {
+            InitializeComponent();
+            random = new Random();
+            btCloseChildForm.Visible = false;
+            this.Text = string.Empty;
+            this.ControlBox = false;
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        //Methods
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    btCloseChildForm.Visible = true;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPane.Controls.Add(childForm);
+            this.panelDesktopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lbTitle.Text = childForm.Text;
+        }
+
+        private void btDangXuat_Click(object sender, EventArgs e)
+        {
+            isThoat = false;
+            this.Close();
+            FormDangNhap f = new FormDangNhap();
+            f.ShowDialog();
+        }
+
+        private void FormChuongTrinh_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (isThoat) 
+            Application.Exit();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormLoaiTangCa(), sender);
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Form2(), sender);
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Form3(), sender);
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormPhanQuyen(), sender);
+        }
+
+        private void btPhongBan_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormPhongBan(), sender);
+        }
+
+        private void btCloseChildForm_Click(object sender, EventArgs e)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            DisableButton();
+            lbTitle.Text = "HOME";
+            panelTitleBar.BackColor = Color.FromArgb(0, 150, 136);
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            currentButton = null;
+            btCloseChildForm.Visible = false;
+        }
+
+        private void panelLogo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panelDesktopPane_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panelMenu_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btMaxMinSize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btMiniSize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panelDesktopPane_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lbNhom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormChuongTrinh_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ CuoiKi/CuoiKi/FormDangNhap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuoiki
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult tl = MessageBox.Show("Are you sure about that? ",
+                 "Thông báo",
+                 MessageBoxButtons.YesNo,
+                 MessageBoxIcon.Question);
+            if (tl == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btDangNhap_Click(object sender, EventArgs e)
+        {
+            FormChuongTrinh f = new FormChuongTrinh();
+            f.Show();
+            this.Hide();
+        }
+
+
+        private void txtTaiKhoan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) 
+                txtMatKhau.Focus();
+        }
+
+        private void txtMatKhau_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+                btDangNhap.Focus();
+        }
+
+        private void FormDangNhap_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
