49255dd798178bc7367653356425916f14cf2b24
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-04-26 22:18:13 +0700

    up

---
+++ ProjectHRM/ProjectHRM/FormDangNhap.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ProjectHRM
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btThoat = new System.Windows.Forms.Button();
+            this.lbTaiKhoan = new System.Windows.Forms.Label();
+            this.lbMatKhau = new System.Windows.Forms.Label();
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btDangNhap = new System.Windows.Forms.Button();
+            this.checkBoxGhiNho = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btThoat
+            // 
+            this.btThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.btThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThoat.ForeColor = System.Drawing.SystemColors.Control;
+            this.btThoat.Location = new System.Drawing.Point(508, 259);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(132, 55);
+            this.btThoat.TabIndex = 1;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = false;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // lbTaiKhoan
+            // 
+            this.lbTaiKhoan.AutoSize = true;
+            this.lbTaiKhoan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.lbTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTaiKhoan.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbTaiKhoan.Location = new System.Drawing.Point(74, 59);
+            this.lbTaiKhoan.Name = "lbTaiKhoan";
+            this.lbTaiKhoan.Size = new System.Drawing.Size(128, 29);
+            this.lbTaiKhoan.TabIndex = 3;
+            this.lbTaiKhoan.Text = "Tài Khoản";
+            // 
+            // lbMatKhau
+            // 
+            this.lbMatKhau.AutoSize = true;
+            this.lbMatKhau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.lbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatKhau.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbMatKhau.Location = new System.Drawing.Point(81, 159);
+            this.lbMatKhau.Name = "lbMatKhau";
+            this.lbMatKhau.Size = new System.Drawing.Size(121, 29);
+            this.lbMatKhau.TabIndex = 4;
+            this.lbMatKhau.Text = "Mật Khẩu";
+            // 
+            // txtTaiKhoan
+            // 
+            this.txtTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTaiKhoan.HideSelection = false;
+            this.txtTaiKhoan.Location = new System.Drawing.Point(282, 59);
+            this.txtTaiKhoan.Multiline = true;
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(345, 29);
+            this.txtTaiKhoan.TabIndex = 5;
+            this.txtTaiKhoan.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTaiKhoan_KeyPress);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(282, 159);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(345, 29);
+            this.txtMatKhau.TabIndex = 6;
+            this.txtMatKhau.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMatKhau_KeyPress);
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.btDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDangNhap.ForeColor = System.Drawing.SystemColors.Control;
+            this.btDangNhap.Location = new System.Drawing.Point(294, 259);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(132, 55);
+            this.btDangNhap.TabIndex = 7;
+            this.btDangNhap.Text = "Đăng nhập";
+            this.btDangNhap.UseVisualStyleBackColor = false;
+            this.btDangNhap.Click += new System.EventHandler(this.btDangNhap_Click);
+            // 
+            // checkBoxGhiNho
+            // 
+            this.checkBoxGhiNho.AutoSize = true;
+            this.checkBoxGhiNho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.checkBoxGhiNho.ForeColor = System.Drawing.SystemColors.Control;
+            this.checkBoxGhiNho.Location = new System.Drawing.Point(79, 214);
+            this.checkBoxGhiNho.Name = "checkBoxGhiNho";
+            this.checkBoxGhiNho.Size = new System.Drawing.Size(74, 20);
+            this.checkBoxGhiNho.TabIndex = 9;
+            this.checkBoxGhiNho.Text = "Ghi nhớ";
+            this.checkBoxGhiNho.UseVisualStyleBackColor = false;
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(670, 349);
+            this.Controls.Add(this.checkBoxGhiNho);
+            this.Controls.Add(this.btDangNhap);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.lbMatKhau);
+            this.Controls.Add(this.lbTaiKhoan);
+            this.Controls.Add(this.btThoat);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "FormDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormDangNhap_FormClosed);
+            this.Load += new System.EventHandler(this.FormDangNhap_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Label lbTaiKhoan;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.TextBox txtTaiKhoan;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.CheckBox checkBoxGhiNho;
+    }
+}
