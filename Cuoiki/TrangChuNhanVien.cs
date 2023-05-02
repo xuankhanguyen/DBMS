3b74fcc5fb48440ca2cc68c0e537c9b3334794d9
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-05-02 11:03:04 +0700

    UP

---
+++ Cuoiki/TrangChuNhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuoiki
+{
+    public partial class TrangChuNhanVien : Form
+    {
+        public TrangChuNhanVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
