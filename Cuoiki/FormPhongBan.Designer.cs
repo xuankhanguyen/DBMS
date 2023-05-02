d4ed2a0e41181a872b837b9a12eb2cba8144a5e1
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-05-02 09:24:19 +0700

    up

--- Cuoiki/FormPhongBan.Designer.cs
+++ Cuoiki/FormPhongBan.Designer.cs
@@ -44,13 +44,23 @@ namespace Cuoiki.Forms
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbMaPB
             // 
             this.lbMaPB.AutoSize = true;
-            this.lbMaPB.Location = new System.Drawing.Point(120, 78);
+            this.lbMaPB.Location = new System.Drawing.Point(56, 19);
             this.lbMaPB.Name = "lbMaPB";
             this.lbMaPB.Size = new System.Drawing.Size(20, 16);
             this.lbMaPB.TabIndex = 12;
@@ -59,10 +69,11 @@ namespace Cuoiki.Forms
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(68, 11);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(290, 23);
             this.button1.TabIndex = 14;
             this.button1.Text = "Thêm";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,10 +81,11 @@ namespace Cuoiki.Forms
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(222, 11);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 23);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(290, 23);
             this.button2.TabIndex = 15;
             this.button2.Text = "Xóa";
             this.button2.UseVisualStyleBackColor = true;
@@ -81,10 +93,11 @@ namespace Cuoiki.Forms
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(381, 11);
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 46);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(290, 23);
             this.button3.TabIndex = 16;
             this.button3.Text = "Sửa";
             this.button3.UseVisualStyleBackColor = true;
@@ -92,7 +105,7 @@ namespace Cuoiki.Forms
             // 
             // txtMaPB
             // 
-            this.txtMaPB.Location = new System.Drawing.Point(273, 78);
+            this.txtMaPB.Location = new System.Drawing.Point(209, 19);
             this.txtMaPB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMaPB.Name = "txtMaPB";
             this.txtMaPB.Size = new System.Drawing.Size(100, 22);
@@ -100,7 +113,7 @@ namespace Cuoiki.Forms
             // 
             // txtTenPB
             // 
-            this.txtTenPB.Location = new System.Drawing.Point(273, 126);
+            this.txtTenPB.Location = new System.Drawing.Point(209, 60);
             this.txtTenPB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTenPB.Name = "txtTenPB";
             this.txtTenPB.Size = new System.Drawing.Size(100, 22);
@@ -108,7 +121,7 @@ namespace Cuoiki.Forms
             // 
             // txtTruongPhong
             // 
-            this.txtTruongPhong.Location = new System.Drawing.Point(273, 172);
+            this.txtTruongPhong.Location = new System.Drawing.Point(577, 19);
             this.txtTruongPhong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTruongPhong.Name = "txtTruongPhong";
             this.txtTruongPhong.Size = new System.Drawing.Size(100, 22);
@@ -116,7 +129,7 @@ namespace Cuoiki.Forms
             // 
             // dtpNgayNhanChuc
             // 
-            this.dtpNgayNhanChuc.Location = new System.Drawing.Point(563, 124);
+            this.dtpNgayNhanChuc.Location = new System.Drawing.Point(577, 60);
             this.dtpNgayNhanChuc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpNgayNhanChuc.Name = "dtpNgayNhanChuc";
             this.dtpNgayNhanChuc.Size = new System.Drawing.Size(200, 22);
@@ -126,7 +139,7 @@ namespace Cuoiki.Forms
             // lbTenPB
             // 
             this.lbTenPB.AutoSize = true;
-            this.lbTenPB.Location = new System.Drawing.Point(120, 126);
+            this.lbTenPB.Location = new System.Drawing.Point(56, 60);
             this.lbTenPB.Name = "lbTenPB";
             this.lbTenPB.Size = new System.Drawing.Size(100, 16);
             this.lbTenPB.TabIndex = 24;
@@ -135,7 +148,7 @@ namespace Cuoiki.Forms
             // lbTruongPhong
             // 
             this.lbTruongPhong.AutoSize = true;
-            this.lbTruongPhong.Location = new System.Drawing.Point(120, 172);
+            this.lbTruongPhong.Location = new System.Drawing.Point(424, 19);
             this.lbTruongPhong.Name = "lbTruongPhong";
             this.lbTruongPhong.Size = new System.Drawing.Size(91, 16);
             this.lbTruongPhong.TabIndex = 25;
@@ -144,7 +157,7 @@ namespace Cuoiki.Forms
             // lbTGNhanChuc
             // 
             this.lbTGNhanChuc.AutoSize = true;
-            this.lbTGNhanChuc.Location = new System.Drawing.Point(411, 126);
+            this.lbTGNhanChuc.Location = new System.Drawing.Point(425, 62);
             this.lbTGNhanChuc.Name = "lbTGNhanChuc";
             this.lbTGNhanChuc.Size = new System.Drawing.Size(126, 16);
             this.lbTGNhanChuc.TabIndex = 26;
@@ -153,20 +166,22 @@ namespace Cuoiki.Forms
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(68, 241);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(748, 241);
+            this.dataGridView1.Size = new System.Drawing.Size(577, 396);
             this.dataGridView1.TabIndex = 27;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(542, 11);
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 69);
             this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(290, 23);
             this.button4.TabIndex = 28;
             this.button4.Text = "Tìm kiếm";
             this.button4.UseVisualStyleBackColor = true;
@@ -174,41 +189,82 @@ namespace Cuoiki.Forms
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(707, 11);
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 92);
             this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(290, 23);
             this.button5.TabIndex = 29;
             this.button5.Text = "Thống kê";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(1, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtMaPB);
+            this.splitContainer1.Panel1.Controls.Add(this.lbTGNhanChuc);
+            this.splitContainer1.Panel1.Controls.Add(this.lbMaPB);
+            this.splitContainer1.Panel1.Controls.Add(this.dtpNgayNhanChuc);
+            this.splitContainer1.Panel1.Controls.Add(this.lbTruongPhong);
+            this.splitContainer1.Panel1.Controls.Add(this.txtTenPB);
+            this.splitContainer1.Panel1.Controls.Add(this.lbTenPB);
+            this.splitContainer1.Panel1.Controls.Add(this.txtTruongPhong);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(871, 504);
+            this.splitContainer1.SplitterDistance = 104;
+            this.splitContainer1.TabIndex = 30;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button5);
+            this.splitContainer2.Panel1.Controls.Add(this.button4);
+            this.splitContainer2.Panel1.Controls.Add(this.button3);
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Size = new System.Drawing.Size(871, 396);
+            this.splitContainer2.SplitterDistance = 290;
+            this.splitContainer2.TabIndex = 0;
             // 
             // FormPhongBan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 505);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.lbTGNhanChuc);
-            this.Controls.Add(this.lbTruongPhong);
-            this.Controls.Add(this.lbTenPB);
-            this.Controls.Add(this.dtpNgayNhanChuc);
-            this.Controls.Add(this.txtTruongPhong);
-            this.Controls.Add(this.txtTenPB);
-            this.Controls.Add(this.txtMaPB);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.lbMaPB);
+            this.Controls.Add(this.splitContainer1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormPhongBan";
             this.Text = "Phòng ban nhân viên";
             this.Load += new System.EventHandler(this.FormPhongBan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -228,5 +284,7 @@ namespace Cuoiki.Forms
         private DataGridView dataGridView1;
         private Button button4;
         private Button button5;
+        private SplitContainer splitContainer1;
+        private SplitContainer splitContainer2;
     }
 }
